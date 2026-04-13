7f49f7ea96c6a2363d8bc2cc7331b868b00714d3
Akchhat <akchhatawasthi1234@gmail.com>
2026-04-13 15:28:20 +0530

    google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import { Providers } from './providers';
 import Header from '@/components/Header';
@@ -28,6 +29,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-VEENV6ZQVP" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-VEENV6ZQVP');
+          `}
+        </Script>
+      </head>
       <body>
         <Providers>
           <div className="min-h-screen bg-background">
